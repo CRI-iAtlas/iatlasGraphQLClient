b394f39ded1b2fb2ce61cae9ffc4d917372df991
andrewelamb <andrewelamb@gmail.com>
2020-09-18 10:06:14 -0700

    added driver results query

---
+++ R/api_driver_result_queries.R
@@ -0,0 +1,91 @@
+utils::globalVariables(".")
+
+#' Query Driver Results
+#'
+#' @param datasets A vector of strings
+#' @param tags A vector of strings
+#' @param features A vector of strings
+#' @param entrez A vector of strings
+#' @param mutation_codes A vector of strings
+#' @param min_p_value A double
+#' @param max_p_value A double
+#' @param min_log10_p_value A double
+#' @param max_log10_p_value A double
+#' @param min_fold_change A double
+#' @param min_log10_fold_change A double
+#' @param min_num_wild_types An integer
+#' @param min_num_mutants An integer
+#' @param page An integer
+#' @param ... Arguments to create_result_from_api_query
+#'
+#' @export
+query_driver_results <- function(
+  datasets = NA,
+  tags = NA,
+  features = NA,
+  entrez = NA,
+  mutation_codes = NA,
+  min_p_value = NA,
+  max_p_value = NA,
+  min_log10_p_value = NA,
+  max_log10_p_value = NA,
+  min_fold_change = NA,
+  min_log10_fold_change = NA,
+  min_num_wild_types = NA,
+  min_num_mutants = NA,
+  page = NA,
+  ...
+){
+  tbl <- create_result_from_paginated_api_query(
+    query_args =  list(
+      dataSet = datasets,
+      tag = tags,
+      feature = features,
+      entrez = entrez,
+      mutationCode = mutation_codes,
+      minPValue = min_p_value,
+      maxPValue = max_p_value,
+      minLog10PValue = min_log10_p_value,
+      maxLog10PValue = max_log10_p_value,
+      minFoldChange = min_fold_change,
+      minLog10FoldChange = min_log10_fold_change,
+      minNumWildTypes = min_num_wild_types,
+      minNumMutants = min_num_mutants,
+      page = page
+    ),
+    query_file = "driver_results.txt",
+    default_tbl = dplyr::tibble(
+      "dataset_name" = character(),
+      "dataset_display" = character(),
+      "feature_name" = character(),
+      "feature_display" = character(),
+      "tag_name" = character(),
+      "tag_display" = character(),
+      "entrez" = integer(),
+      "hgnc" = character(),
+      "p_value" = double(),
+      "fold_change" = double(),
+      "log10_p_value" = double(),
+      "log10_fold_change" = double(),
+      "num_mutant" = integer(),
+      "num_wild_types" = integer(),
+    ),
+    select_cols = c(
+      "dataset_name" = "dataSet.name",
+      "dataset_display" = "dataSet.display",
+      "feature_name" = "feature.name",
+      "feature_display" = "feature.display",
+      "tag_name" = "tag.name",
+      "tag_display" = "tag.display",
+      "entrez" = "gene.entrez",
+      "hgnc" = "gene.hgnc",
+      "p_value" = "pValue",
+      "fold_change" = "foldChange",
+      "log10_p_value" = "log10PValue",
+      "log10_fold_change" = "log10FoldChange",
+      "num_mutant" = "numMutants",
+      "num_wild_types" = "numWildTypes"
+    ),
+    ...
+  )
+}
---
+++ tests/testthat/test-api_driver_result_queries.R
@@ -0,0 +1,36 @@
+query_dir  <- system.file("queries", package = "iatlas.api.client")
+
+test_that("query_driver_results",{
+  expected_columns <- c(
+    "dataset_name",
+    "dataset_display",
+    "feature_name",
+    "feature_display",
+    "tag_name",
+    "tag_display",
+    "entrez",
+    "hgnc",
+    "p_value",
+    "fold_change",
+    "log10_p_value",
+    "log10_fold_change",
+    "num_mutant",
+    "num_wild_types"
+  )
+  result1 <- query_driver_results(
+    datasets = "TCGA",
+    tags = "C1",
+    features = "leukocyte_fraction",
+    query_dir = query_dir
+  )
+  result2 <- query_driver_results(
+    datasets = "none",
+    tags = "C1",
+    features = "leukocyte_fraction",
+    query_dir = query_dir
+  )
+  expect_named(result1, expected_columns)
+  expect_named(result2, expected_columns)
+  expect_true(nrow(result1) > 0)
+  expect_equal(nrow(result2), 0)
+})
