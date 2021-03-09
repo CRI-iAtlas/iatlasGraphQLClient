4782b6f4af4708728ba5a4115098b9028b70e10c
Andrew Lamb <andrewelamb@gmail.com>
2021-03-09 09:08:33 -0800

    add colocalization query

---
+++ R/api_colocalization_queries.R
@@ -0,0 +1,86 @@
+
+#' Query Colocalizations
+#'
+#' @param datasets A vector of strings
+#' @param features A vector of strings
+#' @param coloc_datasets A vector of strings
+#' @param entrez A vector of integers
+#' @param snps A vector of strings
+#' @param qtl_type A string
+#' @param ecaviar_pp A string
+#' @param plot_type A string
+#' @param paging A named list
+#' @param ... Arguments to create_result_from_api_query
+#'
+#' @export
+query_colocalizations <- function(
+  datasets = NA,
+  coloc_datasets = NA,
+  features = NA,
+  entrez = NA,
+  snps = NA,
+  qtl_type = NA,
+  ecaviar_pp = NA,
+  plot_type = NA,
+  paging = NA,
+  ...
+){
+  tbl <- create_result_from_paginated_api_query(
+    query_args =  list(
+      "dataSet" = datasets,
+      "colocDataSet" = coloc_datasets,
+      "feature" = features,
+      "entrez" = entrez,
+      "snp" = snps,
+      "qtlType" = qtl_type,
+      "eCaviarPP" = ecaviar_pp,
+      "plotType" = plot_type,
+      "paging" = paging,
+      "distinct" = F
+    ),
+    query_file = "colocalizations.txt",
+    default_tbl = dplyr::tibble(
+      "dataset_name" = character(),
+      "dataset_display" = character(),
+      "coloc_dataset_name" = character(),
+      "coloc_dataset_display" = character(),
+      "feature_name" = character(),
+      "feature_display" = character(),
+      "feature_germline_module" = character(),
+      "feature_germline_category" = character(),
+      "snp_name" = character(),
+      "snp_rsid" = character(),
+      "snp_chr" = character(),
+      "snp_bp" = integer(),
+      "gene_entrez" = integer(),
+      "gene_hgnc" = character(),
+      "qtl_type" = character(),
+      "ecaviar_pp" = character(),
+      "plot_type" = character(),
+      "splice_loc" = character(),
+      "plot_link" = character()
+    ),
+    select_cols = c(
+      "dataset_name" = "dataSet.name",
+      "dataset_display" = "dataSet.display",
+      "coloc_dataset_name" = "colocDataSet.name",
+      "coloc_dataset_display" = "colocDataSet.display",
+      "feature_name" = "feature.name",
+      "feature_display" = "feature.display",
+      "feature_germline_module" = "feature.germline_module",
+      "feature_germline_category" = "feature.germline_category",
+      "snp_name" = "snp.name",
+      "snp_rsid" = "snp.rsid",
+      "snp_chr" = "snp.chr",
+      "snp_bp" = "snp.bp",
+      "gene_entrez" = "gene.entrez",
+      "gene_hgnc" = "gene.hgnc",
+      "qtl_type" = "qtlType",
+      "ecaviar_pp" = "eCaviarPP",
+      "plot_type" = "plotType",
+      "splice_loc" = "spliceLoc",
+      "plot_link" = "plotLink"
+    ),
+    ...
+  )
+}
---
+++ tests/testthat/test-api_colocalization_queries.R
@@ -0,0 +1,39 @@
+
+test_that("query_colocalizations",{
+  expected_columns <- c(
+    "dataset_name",
+    "dataset_display",
+    "coloc_dataset_name",
+    "coloc_dataset_display",
+    "feature_name",
+    "feature_display",
+    "feature_germline_module",
+    "feature_germline_category",
+    "snp_name",
+    "snp_rsid",
+    "snp_chr",
+    "snp_bp",
+    "gene_entrez",
+    "gene_hgnc",
+    "qtl_type",
+    "ecaviar_pp",
+    "plot_type",
+    "splice_loc",
+    "plot_link"
+  )
+  result1 <- query_colocalizations(
+    datasets = "TCGA",
+    query_dir = query_dir
+  )
+
+  expect_named(result1, expected_columns)
+  expect_true(nrow(result1) > 0)
+
+  result2 <- query_colocalizations(
+    datasets = "TCGA",
+    snps = "none",
+    query_dir = query_dir
+  )
+  expect_named(result2, expected_columns)
+  expect_equal(nrow(result2), 0)
+})
