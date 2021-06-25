e83610d2a6a57d4a42b08e561a06b72170c1d163
Andrew Lamb <andrewelamb@gmail.com>
2021-06-25 08:19:14 -0700

    reverted back to previous state

--- tests/testthat/test-api_features_queries.R
+++ tests/testthat/test-api_features_queries.R
@@ -24,6 +24,34 @@ test_that("query_features", {
   expect_equal(nrow(result2), 0)
 })
 
+test_that("query_feature_values", {
+  expected_columns <- c(
+    "value",
+    "sample_name",
+    "feature_name",
+    "feature_display",
+    "feature_order",
+    "feature_class"
+  )
+
+  result1 <- query_feature_values(
+    cohorts = "PCAWG_Immune_Subtype",
+    features = "Lymphocytes_Aggregate1",
+    query_dir = query_dir
+  )
+  result2 <- query_feature_values(
+    cohorts = "PCAWG_Immune_Subtype",
+    features = "not_a_feature",
+    query_dir = query_dir
+  )
+
+  expect_named(result1, expected_columns)
+  expect_named(result2, expected_columns)
+
+  expect_true(nrow(result1) > 0)
+  expect_equal(nrow(result2), 0)
+})
+
 test_that("query_features_range", {
   expected_columns <- c("name", "display", "value_min", "value_max")
   result1 <- query_features_range(
