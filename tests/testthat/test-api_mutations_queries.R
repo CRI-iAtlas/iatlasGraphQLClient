602a18f21432efd0d45b9c4d401adfe1b2eb6d8a
Andrew Lamb <andrewelamb@gmail.com>
2021-07-15 10:25:16 -0700

    update mutation and driver result queries

--- tests/testthat/test-api_mutations_queries.R
+++ tests/testthat/test-api_mutations_queries.R
@@ -1,39 +1,38 @@
 
 test_that("query_mutations", {
   expected_columns <-  c(
-    "mutation_id",
-    "gene_entrez",
-    "gene_hgnc",
+    "mutation_name",
     "mutation_code",
     "mutation_type_name",
     "mutation_type_display",
-    "mutation_name"
+    "gene_entrez",
+    "gene_hgnc"
   )
-  result1 <- query_mutations(ids = 1, query_dir = query_dir)
-  result2 <- query_mutations(entrez = -1, query_dir = query_dir)
+  result1 <- query_mutations(mutations = 'ABL1:(NS)', query_dir = query_dir)
   expect_named(result1, expected_columns)
-  expect_named(result2, expected_columns)
   expect_true(nrow(result1) > 0)
+
+  result2 <- query_mutations(mutations = 'not_a_mutation', query_dir = query_dir)
+  expect_named(result2, expected_columns)
   expect_equal(nrow(result2), 0)
 })
 
 test_that("query_mutation_statuses", {
   expected_columns <-  c(
-    "mutation_id",
     "mutation_name",
-    "gene_entrez",
-    "gene_hgnc",
     "mutation_code",
     "mutation_type_name",
     "mutation_type_display",
+    "gene_entrez",
+    "gene_hgnc",
     "sample_name",
     "mutation_status"
   )
-  result1 <- query_mutation_statuses(ids = 1, query_dir = query_dir)
+  result1 <- query_mutation_statuses(mutations = 'ABL1:(NS)', query_dir = query_dir)
   expect_named(result1, expected_columns)
   expect_true(nrow(result1) > 0)
 
-  result2 <- query_mutation_statuses(entrez = -1, query_dir = query_dir)
+  result2 <- query_mutation_statuses(mutations = 'not_a_mutation', query_dir = query_dir)
   expect_named(result2, expected_columns)
   expect_equal(nrow(result2), 0)
 })
