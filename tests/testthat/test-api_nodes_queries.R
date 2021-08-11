21cd650c9193e956f2bf139800d77542e1e64164
Andrew Lamb <andrewelamb@gmail.com>
2021-08-11 12:17:49 -0700

    fixed issues with nodes query

--- tests/testthat/test-api_nodes_queries.R
+++ tests/testthat/test-api_nodes_queries.R
@@ -3,6 +3,8 @@ query_dir  <- system.file("queries", package = "iatlas.api.client")
 test_that("query_gene_nodes",{
   expected_columns <- c(
     get_node_field_names(),
+    "feature_name",
+    "feature_display",
     "gene_entrez",
     "gene_hgnc",
     "gene_friendly_name"
@@ -21,8 +23,8 @@ test_that("query_gene_nodes",{
     network = "Extracellular Network",
     query_dir = query_dir
   )
-  expect_named(result1, expected_columns)
-  expect_named(result2, get_node_field_names())
+  expect_named(result1, expected_columns, ignore.order = T)
+  expect_named(result2, expected_columns, ignore.order = T)
   expect_true(nrow(result1) > 0)
   expect_equal(nrow(result2), 0)
 })
@@ -31,7 +33,10 @@ test_that("query_feature_nodes",{
   expected_columns <- c(
     get_node_field_names(),
     "feature_name",
-    "feature_display"
+    "feature_display",
+    "gene_entrez",
+    "gene_hgnc",
+    "gene_friendly_name"
   )
   result1 <- query_nodes(
     datasets = "TCGA",
@@ -40,17 +45,21 @@ test_that("query_feature_nodes",{
     network = "Extracellular Network",
     query_dir = query_dir
   )
-  expect_named(result1, expected_columns)
+  expect_named(result1, expected_columns, ignore.order = T)
   expect_true(nrow(result1) > 0)
 
   result2 <- query_nodes(
-    datasets = "none",
+    datasets = "TCGA",
     tags = "C1",
-    features =  "B_cells_Aggregate2",
+    features = c(
+      "B_cells_Aggregate2",
+      "Dendritic_cells_Aggregate2"
+    ),
     network = "Extracellular Network",
+    min_score = 5,
     query_dir = query_dir
   )
-  expect_named(result2, get_node_field_names())
+  expect_named(result2, expected_columns, ignore.order = T)
   expect_equal(nrow(result2), 0)
 
 })
