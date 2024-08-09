f9c80ad681f86db1b97ee0592e4e2196ded34959
Andrew Lamb <andrewelamb@gmail.com>
2024-08-09 09:48:24 -0700

    add new query

--- R/api_cells_queries.R
+++ R/api_cells_queries.R
@@ -31,3 +31,51 @@ query_cells <- function(
   )
   return(tbl)
 }
+
+#' Query Feature Values
+#'
+#' @param cohorts A vector of strings
+#' @param cells A vector of strings
+#' @param paging A named list
+#' @param ... Arguments to create_result_from_api_query
+#' @export
+#' @importFrom magrittr %>%
+query_cell_feature_values <- function(
+    cohorts = NA,
+    cells = NA,
+    paging = NA,
+    ...
+){
+  tbl <- create_result_from_cursor_paginated_api_query(
+    query_args =  list(
+      "cohort" = cohorts,
+      "cell" = cells,
+      "paging" = paging,
+      "distinct" = F
+    ),
+    query_file = "cell_feature_values.txt",
+    default_tbl = dplyr::tibble(
+      "cell_name" = character(),
+      "cell_type" = character(),
+      "feature_name" = character(),
+      "feature_value" = double(),
+    ),
+    select_cols = c(
+      "cell_name" = "name",
+      "cell_type" = "type",
+      "features"
+    ),
+    ...
+  )
+  if(nrow(tbl) == 0) return(tbl)
+  else {
+    tbl %>%
+      tidyr::unnest(cols = "features", keep_empty = T) %>%
+      dplyr::select(
+        "cell_name",
+        "cell_type",
+        "feature_name" = "name",
+        "feature_value" = "value"
+      )
+  }
+}
--- tests/testthat/test-api_cells_queries.R
+++ tests/testthat/test-api_cells_queries.R
@@ -13,3 +13,74 @@ test_that("query_cells", {
   expect_true(nrow(result2) == 0)
 
 })
+
+test_that("query_cell_feature_values", {
+  expected_columns <- c(
+    "cell_name",
+    "cell_type",
+    "feature_name",
+    "feature_value"
+  )
+  
+  result1 <- query_cell_feature_values(
+    cells = c('RU1311A_T_1_165945547864806'),
+    query_dir = query_dir
+  )
+  expect_named(result1, expected_columns)
+  expect_true(nrow(result1) > 0)
+  
+  result2 <- query_cell_feature_values(
+    cells = c('not_a_cell'),
+    query_dir = query_dir
+  )
+  expect_named(result2, expected_columns)
+  expect_equal(nrow(result2), 0)
+})
+
+#' Query Feature Values
+#'
+#' @param cohorts A vector of strings
+#' @param cells A vector of strings
+#' @param paging A named list
+#' @param ... Arguments to create_result_from_api_query
+#' @export
+#' @importFrom magrittr %>%
+query_cell_feature_values <- function(
+    cohorts = NA,
+    cells = NA,
+    paging = NA,
+    ...
+){
+  tbl <- create_result_from_cursor_paginated_api_query(
+    query_args =  list(
+      "cohort" = cohorts,
+      "cell" = cells,
+      "paging" = paging,
+      "distinct" = F
+    ),
+    query_file = "cell_feature_values.txt",
+    default_tbl = dplyr::tibble(
+      "cell_name" = character(),
+      "cell_type" = character(),
+      "feature_name" = character(),
+      "feature_value" = double(),
+    ),
+    select_cols = c(
+      "cell_name" = "name",
+      "cell_type" = "type",
+      "features"
+    ),
+    ...
+  )
+  if(nrow(tbl) == 0) return(tbl)
+  else {
+    tbl %>%
+      tidyr::unnest(cols = "features", keep_empty = T) %>%
+      dplyr::select(
+        "cell_name",
+        "cell_type",
+        "feature_name" = "name",
+        "feature_value" = "value"
+      )
+  }
+}
